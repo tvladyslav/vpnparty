e139ed49f3bde134126521aeb5e88503d460ce25
Vladyslav Tsilytskyi <ykp@protonmail.ch>
2024-09-24 01:45:52 +0200

    Cosmetic changes in generator

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,5 +1,4 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
-use std::time::Duration;
+use std::net::{Ipv4Addr, SocketAddr, UdpSocket};
 
 const WARCRAFT3_PORT: u16 = 6112;
 const WARCRAFT3_PAYLOAD: [u8; 16] = [
@@ -10,21 +9,20 @@ const TORCHLIGHT2_PORT: u16 = 4549;
 const TORCHLIGHT2_PAYLOAD: [u8; 7] = [0xab, 0x84, 0x54, 0x72, 0x2c, 0x00, 0x00];
 
 fn main() -> std::io::Result<()> {
-    let local_ip = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
-    let broadcast_ip = IpAddr::V4(Ipv4Addr::new(255, 255, 255, 255));
-
-    let wc3_dest: SocketAddr = SocketAddr::from((broadcast_ip, WARCRAFT3_PORT));
-    let wc3_socket: UdpSocket = UdpSocket::bind(SocketAddr::from((local_ip, WARCRAFT3_PORT)))?;
+    let wc3_dest: SocketAddr = SocketAddr::from((Ipv4Addr::BROADCAST, WARCRAFT3_PORT));
+    let wc3_socket: UdpSocket =
+        UdpSocket::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, WARCRAFT3_PORT)))?;
     wc3_socket.set_broadcast(true)?;
 
-    let tl2_dest: SocketAddr = SocketAddr::from((broadcast_ip, TORCHLIGHT2_PORT));
-    let tl2_socket: UdpSocket = UdpSocket::bind(SocketAddr::from((local_ip, TORCHLIGHT2_PORT)))?;
+    let tl2_dest: SocketAddr = SocketAddr::from((Ipv4Addr::BROADCAST, TORCHLIGHT2_PORT));
+    let tl2_socket: UdpSocket =
+        UdpSocket::bind(SocketAddr::from((Ipv4Addr::UNSPECIFIED, TORCHLIGHT2_PORT)))?;
     tl2_socket.set_broadcast(true)?;
 
     loop {
         let _wc3_bytes_sent = wc3_socket.send_to(&WARCRAFT3_PAYLOAD, wc3_dest)?;
         let _tl2_bytes_sent = tl2_socket.send_to(&TORCHLIGHT2_PAYLOAD, tl2_dest)?;
-        // println!("Sent {} bytes.", _tl2_bytes_sent);
-        std::thread::sleep(Duration::from_secs(1));
+        // println!("Sent {} bytes.", _wc3_bytes_sent + _tl2_bytes_sent);
+        std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
